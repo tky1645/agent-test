eb2813f363304909f93f8ac37e76d79354012b3b
tak <tak@gmail.com>
2025-02-15 15:58:39 +0900

    feat: set up router

---
+++ DDD/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"DDD/command/user"
+	"github.com/gin-gonic/gin"
+)
+
+
+func main (){
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, "ping pong")
+	})
+	r.GET("/users", user.Handler)
+
+	r.Run(":8080")
+}
