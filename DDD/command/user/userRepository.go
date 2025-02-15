f2474ba350f8612da9df0c570b82504f6d140fe9
tak <tak@gmail.com>
2025-02-15 17:18:09 +0900

    feat: add user factory method

--- DDD/command/user/userRepository.go
+++ DDD/command/user/userRepository.go
@@ -4,6 +4,11 @@ import "DDD/entities"
 
 var _ IUserRepository = (*UserRepository)(nil)
 
+type userTable struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 type UserRepository struct {
 }
 
@@ -16,5 +21,13 @@ func (r *UserRepository) Save(user entities.User) error {
 }
 
 func (r *UserRepository) Create(id int) entities.User {
-	 return entities.NewUser(id, "createJohn")
+	user := r.get(id)
+	return entities.NewUser(user.ID, user.Name)
+}
+
+func (r *UserRepository) get(id int) userTable{
+	return userTable{
+		ID:   id,
+		Name: "getJohn",
+	}
 }
--- DDD/entities/user.go
+++ DDD/entities/user.go
@@ -1,13 +1,31 @@
 package entities
 
+import "fmt"
+
 type User struct {
 	ID   int    `json:"id"`
-	Name string `json:"name"`
+	Name userName `json:"name"`
 }
 
-func NewUser(id int, name string) User {
+
+type userName string
+func newUserName(name string) (userName, error) {
+	if name == "" {
+		return "",fmt.Errorf("name is empty")
+	}
+
+	return userName(name),nil
+}
+
+// factory method
+func NewUser(id int, name string)( User, error) {
+	userName, err := newUserName(name)
+	if err != nil {
+		return User{}, err
+	}
+
 	return User{
 		ID:   id,
-		Name: name,
-	}
+		Name: userName,
+	},nil
 }
